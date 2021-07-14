9831e47f943164769c3a2a5b0912ab1faaa6675d
Guilherme de Carvalho Souza <81447194+gui-souza@users.noreply.github.com>
2021-07-14 18:31:04 -0300

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Calculator from "../components/Calculator";
+import Header from "../components/Header";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <Calculator />
+    </>
+  );
+}
+
+export default HomePage;
